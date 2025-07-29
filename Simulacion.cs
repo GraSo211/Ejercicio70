8a1845540c06822f0b0e632a46c374225ab0ff8d
Santiago <santigs211@gmail.com>
2025-07-28 23:59:13 -0300

    Refactor Simulacion class and Vector_Estado struct
    Updated the Simulacion class in the Ejercicio70 namespace by adding new properties for professors and queues, and initializing them in the constructor. Simplified the IniciarSimulacion method by removing redundant local variables. Enhanced encapsulation in the Vector_Estado struct by making properties private.

--- Simulacion.cs
+++ Simulacion.cs
@@ -17,11 +17,19 @@ namespace Ejercicio70
             private float porcAprobacionTeo { get; set; }
             private int horaFinPract { get; set; }
             private int horaFinExamen { get; set; }
+            
+
 
             private float reloj { get; set; }
-            private List<Evento> eventos { get; set; }
-
+            private Profesor adjunto1 { get; set; }
+            private Profesor adjunto2 { get; set; }
+            private Profesor titularCatedra { get; set; }
+            private Cola colaAdjuntos { get; set; }
+            private Cola colaTitular { get; set; }
+            private float acumTiempoTotalExamenAlumnosAprobados { get; set; }
+            private int contAlumnosAprobados { get; set; }
             private List<Cliente> listaClientes { get; set; }
+            private List<Evento> eventos { get; set; }
             
 
         public Simulacion(int cantidadAlumnos, float finPartePractica, float finCorreccionPartePracticaA, float finCorreccionPartePracticaB, float finCorreccionParteTeorica, float porcAprobacionPract, float porcAprobacionTeo, int horaFinPract, int horaFinExamen)
@@ -35,6 +43,19 @@ namespace Ejercicio70
                 this.porcAprobacionTeo = porcAprobacionTeo;
                 this.horaFinPract = horaFinPract;
                 this.horaFinExamen = horaFinExamen;
+
+
+                this.reloj = horaFinPract;
+                
+                this.colaAdjuntos = new Cola(1, "Cola_Profesores_Adjuntos");
+                this.colaTitular = new Cola(2, "Cola_Titular_Catedra");
+                this.adjunto1 = new Profesor(1, "Adjunto_1", colaAdjuntos);
+                this.adjunto2 = new Profesor(2, "Adjunto_2", colaAdjuntos);
+                this.titularCatedra = new Profesor(3, "Titular_Catedra", colaTitular);
+                this.acumTiempoTotalExamenAlumnosAprobados = 0;
+                this.contAlumnosAprobados = 0;
+                this.listaClientes = new List<Cliente>();
+                this.eventos = new List<Evento>();
         }
 
 
@@ -46,12 +67,7 @@ namespace Ejercicio70
                     listaClientes.Add(cliente);
 
                 }
-                Cola colaAdjuntos = new Cola(1, "Cola_Profesores_Adjuntos");
-                Cola colaTitular = new Cola(2, "Cola_Titular_Catedra");
-
-                Profesor profesorAdjunto1 = new Profesor(1,"Adjunto_1", cola_adjuntos);
-                Profesor profesorAdjunto2 = new Profesor(2, "Adjunto_2", cola_adjuntos);
-                Profesor titularCatedra = new Profesor(3, "Titular_Catedra", cola_titular);
+                
                 
             }
         }                                                         
@@ -59,8 +75,20 @@ namespace Ejercicio70
     
     public struct Vector_Estado
     {
-        public int id { get; set; }
-        public string eventoActual { get; set; }
+        private int id { get; set; }
+        private string eventoActual { get; set; }
+        private float reloj { get; set; }
+        private Evento finPartePractica { get; set; }
+        private Evento finCorreccionPartePracticaAdjunto1 { get; set; }
+        private Evento finCorreccionPartePracticaAdjunto2 { get; set; }
+        private Evento finCorreccionParteTeorica { get; set; }
+        private Evento finExamen { get; set; }
+        private Profesor  adjunto1 { get; set; }
+        private Profesor adjunto2 { get; set; }
+        private Profesor titularCatedra { get; set; }
+        private Cola colaAdjuntos { get; set; }
+        private Cola colaTitular { get; set; }
+
         public Vector_Estado() { }
     }
 }                                                                 
